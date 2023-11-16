a2ef96e9995be74fde7dbf28bd70e7c1b43fc40f
Larissa Mara <larimara.garcia@gmail.com>
2023-11-15 22:31:53 -0300

    paginas atualizar e excluir

--- src/app/atualizar-local-descarte/atualizar-local-descarte.component.ts
+++ src/app/atualizar-local-descarte/atualizar-local-descarte.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-atualizar-local-descarte',
   templateUrl: './atualizar-local-descarte.component.html',
-  styleUrls: ['./atualizar-local-descarte.component.css']
+  styleUrls: ['./atualizar-local-descarte.component.css'],
 })
 export class AtualizarLocalDescarteComponent {
+  exibirResultados: boolean = false;
 
+  recebedores: any = [
+    {
+      nome: 'Facens',
+      telefone: '(15)998123456',
+      email: 'facens@facens.br',
+      endereco:
+        'Rodovia Senador José Ermírio de Moraes, 1425 - Jardim Constantino Matucci, Sorocaba - SP, 18085-784',
+    },
+  ];
+
+  setExibirTrue() {
+    this.exibirResultados = true;
+  }
+  voltar() {
+    window.history.back();
+  }
 }
+
--- src/app/excluir-local-descarte/excluir-local-descarte.component.ts
+++ src/app/excluir-local-descarte/excluir-local-descarte.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-excluir-local-descarte',
   templateUrl: './excluir-local-descarte.component.html',
-  styleUrls: ['./excluir-local-descarte.component.css']
+  styleUrls: ['./excluir-local-descarte.component.css'],
 })
 export class ExcluirLocalDescarteComponent {
-
+  voltar() {
+    window.history.back();
+  }
 }
